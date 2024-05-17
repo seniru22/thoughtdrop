6bb0e7ed709c73ceb8deefb4d7747b7d9e3fcdb9
Shreekar11 <shreekargade2004@gmail.com>
2024-05-18 05:06:07 +0530

    cron added

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -22,9 +22,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 const express_1 = __importStar(require("express"));
 const cors = require("cors");
+const cron = require("node-cron");
 const app = (0, express_1.default)();
 const corsOptions = {
     origin: [
@@ -39,11 +53,49 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((0, express_1.urlencoded)({ extended: true }));
 app.use(express_1.default.json());
-const userRoutes = require('../routes/user.routes');
-const blogRoutes = require('../routes/blog.routes');
+const userRoutes = require("../routes/user.routes");
+const blogRoutes = require("../routes/blog.routes");
 app.use(userRoutes);
 app.use(blogRoutes);
 const port = process.env.PORT || 5000;
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send("Server running!");
+}));
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
 });
+// Added cron
+app.get("/ping", (req, res) => {
+    res.status(200).json("pong....");
+});
+const API_ENDPOINT = "https://blog-platform-vq3i.onrender.com";
+const makeApiRequest = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield axios_1.default.get(API_ENDPOINT);
+        return response.data;
+    }
+    catch (err) {
+        console.error("API request failed:", err.message);
+        throw err;
+    }
+});
+const runApiRequestJob = () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("Running API request job...");
+    try {
+        const responseData = yield makeApiRequest();
+        return responseData;
+    }
+    catch (error) {
+        return null;
+    }
+});
+cron.schedule("* * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
+    const responseData = yield runApiRequestJob();
+    if (responseData) {
+        // Process the response data here
+        console.log("API request successful:", responseData);
+    }
+    else {
+        console.log("API request failed");
+    }
+}));
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,30 +1,74 @@
+import axios from "axios";
 import express, { Request, Response, Application, urlencoded } from "express";
 
 const cors = require("cors");
+const cron = require("node-cron");
 
 const app: Application = express();
 const corsOptions = {
-    origin: [
-      "http://localhost:3000",
-      "https://blog-platform-zeta-five.vercel.app",
-      "https://blog-platform-at2g7koor-shreekar11s-projects.vercel.app",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-  app.use(cors(corsOptions));
+  origin: [
+    "http://localhost:3000",
+    "https://blog-platform-zeta-five.vercel.app",
+    "https://blog-platform-at2g7koor-shreekar11s-projects.vercel.app",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 
-const userRoutes = require('../routes/user.routes');
-const blogRoutes = require('../routes/blog.routes');
+const userRoutes = require("../routes/user.routes");
+const blogRoutes = require("../routes/blog.routes");
 
 app.use(userRoutes);
 app.use(blogRoutes);
 
 const port = process.env.PORT || 5000;
 
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Server running!");
+});
+
 app.listen(port, () => {
-    console.log(`Server running at ${port}`);
+  console.log(`Server running at ${port}`);
+});
+
+// Added cron
+
+app.get("/ping", (req, res) => {
+  res.status(200).json("pong....");
+});
+
+const API_ENDPOINT = "https://blog-platform-vq3i.onrender.com";
+
+const makeApiRequest = async () => {
+  try {
+    const response = await axios.get(API_ENDPOINT);
+    return response.data;
+  } catch (err: any) {
+    console.error("API request failed:", err.message);
+    throw err;
+  }
+};
+
+const runApiRequestJob = async () => {
+  console.log("Running API request job...");
+  try {
+    const responseData = await makeApiRequest();
+    return responseData;
+  } catch (error) {
+    return null;
+  }
+};
+
+cron.schedule("* * * * *", async () => {
+  const responseData = await runApiRequestJob();
+  if (responseData) {
+    // Process the response data here
+    console.log("API request successful:", responseData);
+  } else {
+    console.log("API request failed");
+  }
 });
